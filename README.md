0d82bd64521007a2008dff612efb98c83e48e028
siwony_ <siwon103305@gmail.com>
2021-07-07 22:59:45 +0900

    [INIT] 초기 설정

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# UserAPI
+# BulletinBoardAPI
+### 프로젝트 동기 
+1. kotlin 이 너무하고 싶었습니다.
+2. 언젠가 있을 Java 에서 Kotlin migration를 준비하기 위함입니다.
+
+### TODO
+- USER Authentication with JWT
+- Bulletin Board API
+    - category
---
+++ src/main/kotlin/com/practice/siwony/SiwonyApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.siwony
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SiwonyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SiwonyApplication>(*args)
+}
