7a6d622f976f703aa567f1e763822a5d66bbca09
siwony_ <siwon103305@gmail.com>
2021-07-15 23:27:13 +0900

    [UPDATE] UserRepository에 대한 Repository 생성

--- src/main/kotlin/com/bulletinBoard/siwony/repository/UserRepository.kt
+++ src/main/kotlin/com/bulletinBoard/siwony/repository/UserRepository.kt
@@ -1,4 +1,6 @@
 package com.bulletinBoard.siwony.repository
 
-class UserRepository {
-}
+import com.bulletinBoard.siwony.entity.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<UserEntity, Long>{}
