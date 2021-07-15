5c5c0cff6deff5312cf8706dc4e4885f79c25cba
siwony_ <siwon103305@gmail.com>
2021-07-15 23:49:40 +0900

    [UPDATE] userEntity save 테스트

--- src/test/kotlin/com/bulletinBoard/siwony/entity/UserEntityTest.kt
+++ src/test/kotlin/com/bulletinBoard/siwony/entity/UserEntityTest.kt
@@ -1,11 +1,16 @@
 package com.bulletinBoard.siwony.entity
 
+import com.bulletinBoard.siwony.repository.UserRepository
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
+@DataJpaTest
 internal class UserEntityTest{
+
+    @Autowired lateinit var userRepository: UserRepository
 
     @Test @DisplayName("Entity 테스트")
     fun UserEntit_테스트(){
@@ -24,10 +29,24 @@ internal class UserEntityTest{
         // Then
         assertEquals(Role.ROLE_CLIENT.name, getAuthority)
         assertEquals(PWD, password)
-        assertEquals(true, accountNonExpired)
-        assertEquals(true, accountNonLocked)
-        assertEquals(true, credentialsNonExpired)
-        assertEquals(true, enabled)
+        assertTrue(accountNonExpired)
+        assertTrue(accountNonLocked)
+        assertTrue(credentialsNonExpired)
+        assertTrue(enabled)
+    }
+
+    @Test @DisplayName("UserEntity 저장 테스트")
+    fun UserEntity_save_테스트(){
+        val email = "siwon103305@gmail.com"
+        val password = "password"
+        val nickname = "siwony_"
+
+        val userEntity = UserEntity(email, password, nickname)
+        val save = userRepository.save(userEntity);
+
+        assertEquals(save.username, email);
+        assertEquals(save.password, password)
+        assertEquals(save.nickname, nickname)
     }
 
 }
